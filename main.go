779fb7a2dc7cbe77ff132bfe61d00120c58cce8c
close-leo <close0818@gmail.com>
2025-12-02 23:33:41 +0800

    init

---
+++ main.go
@@ -0,0 +1,20 @@
+package main
+
+import (
+	"github.com/Allenxuxu/gev/log"
+	"github.com/gin-gonic/gin"
+)
+
+func main() {
+	r := gin.Default()
+
+	// GET /ping
+	r.GET("/ping", func(c *gin.Context) {
+		c.JSON(200, gin.H{
+			"message": "pong",
+		})
+	})
+
+	log.Info("Starting server on :8080")
+	r.Run(":8080") // 監聽 :8080
+}
